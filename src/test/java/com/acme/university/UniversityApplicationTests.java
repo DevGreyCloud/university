e3cb35fb442190c1d9eb9758ce99d7cf5724faa0
DevGreyCloud <dev@graumann.io>
2025-07-18 23:24:50 +0200

    chore: initial commit

---
+++ src/test/java/com/acme/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
